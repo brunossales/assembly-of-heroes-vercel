1adcd773f8ef37974de81f7b432ed5cdd6f85550
Bruno Sales <bruno.particular25@hotmail.com>
2023-03-30 23:25:18 -0300

    feat: change test

--- mocks/hero.ts
+++ mocks/hero.ts
@@ -1,8 +1,8 @@
-import { Hero } from "@/src/@core/domain/entities/Hero";
+import { HeroEntity } from "@/src/@core/domain/entities/Hero";
 
 // Mockando hero para testes
-export const mockHero = (): Hero => {
-  return new Hero({
+export const mockHero = (): HeroEntity => {
+  const heroMock: HeroEntity ={
     id: 0,
     name: 'Bruno Hero',
     comics: {available: 0, returned: 0, items: [{name: '', resourceURI: ''}]},
@@ -12,5 +12,7 @@ export const mockHero = (): Hero => {
       path: "https://cdn.pixabay.com/photo/2019/03/21/02/51/deadpool-4070071_960_720",
     },
     description: 'Description'
-  });
+  };
+
+  return heroMock;
 };
